b0be165248c06047301788bb411bbf449bd47397
JakeMilne <j.milne27@rgu.ac.uk>
2024-11-09 13:40:29 +0000

    started messing about with spring, and servlet. need to figure out how to construct the form the right way

---
+++ PythonIDE_Testing/PythonIDE_Testing/src/test/java/com/example/PythonIDE_Testing/PythonIdeTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = PythonIdeTestingApplication.class)
+public class PythonIdeTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Basic context loading test to ensure the application starts up
+	}
+}
