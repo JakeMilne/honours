326dbf51c7c254701c313ddb1c0041e13a0d422e
JakeMilne <j.milne27@rgu.ac.uk>
2025-02-21 10:14:38 +0000

    started cleaning up stuff related to llm calls

--- PythonIDE_Testing/PythonIDE_Testing/src/main/java/com/example/PythonIDE_Testing/LLM.java
+++ PythonIDE_Testing/PythonIDE_Testing/src/main/java/com/example/PythonIDE_Testing/LLM.java
@@ -1,13 +1,22 @@
 package com.example.PythonIDE_Testing;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Map;
+import java.util.List;
+import com.google.gson.Gson;
+
+
 public class LLM {
 
-    private string url;
-    private string model;
+    private String url;
+    private String model;
     private boolean needsKey;
-    private string key;
+    private String key;
 
-    public LLM(string url, string model, boolean needsKey, string key){
+    public LLM(String url, String model, boolean needsKey, String key){
         this.url = url;
         this.model = model;
         this.needsKey = needsKey;
@@ -17,11 +26,11 @@ public class LLM {
 
 
 
-    public string getUrl(){
+    public String getUrl(){
         return url;
     }
 
-    public string getModel(){
+    public String getModel(){
         return model;
     }
 
@@ -29,8 +38,54 @@ public class LLM {
         return needsKey;
     }
 
-    public string getKey(){
+    public String getKey(){
         return key;
     }
 
+    public HttpRequest buildCall(String prompt){
+
+        Gson gson = new Gson();
+        String url = this.url;
+
+
+        Map<String, Object> requestBody = Map.of(
+                "model", this.model,
+                "messages", List.of(
+                        Map.of("role", "system", "content", "You are a coding assistant that creates python code. Only Create python 3.9 code, offer no explanation, do not include anything in your answer other than python code. Only return 1 piece of code. Tag all code with ```python. DO not create any code that is not specified by the user. All code generated must be in the same class and file. All code must be placed inside the same block. Only return python code, and do not provide any additional context or instructions to the user, unless they are in a comment inside the python code. your response must contain exactly 1 block of python code, no more or less. UNDER NO CIRCUMSTANCES WHATSOEVER SHOULD YOU GIVE ME MORE THAN 1 BLOCK OF CODE."),
+
+                        Map.of("role", "user", "content", prompt)
+                )
+        );
+
+        String jsonBody = gson.toJson(requestBody);
+
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                .build();
+
+        if(this.needsKey){
+            request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "Bearer " + this.key)
+                    .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                    .build();
+        }
+
+
+
+        return request;
+    }
+
+    public HttpRequest buildVulnCall(String prompt){
+
+
+
+
+
+        return null;
+    }
 }
--- PythonIDE_Testing/PythonIDE_Testing/src/main/java/com/example/PythonIDE_Testing/codeGenerator.java
+++ PythonIDE_Testing/PythonIDE_Testing/src/main/java/com/example/PythonIDE_Testing/codeGenerator.java
@@ -24,7 +24,7 @@ public class codeGenerator {
     private int iterationCount = 0;
     private String[][] issues;
 
-    private LLM llm = new LLM("http://localhost:1234/v1/chat/completions", "meta-llama-3.1-8b-instruct", false, "");
+//    private LLM llm = new LLM("http://localhost:1234/v1/chat/completions", "meta-llama-3.1-8b-instruct", false, "");
 //    private LLM llm = new LLM("https://api.openai.com/v1/chat/completions", "gpt-4o-mini", true, System.getenv("OPENAI_API_KEY"));
 
     public codeGenerator() {}
@@ -40,43 +40,49 @@ public class codeGenerator {
 
     //getting the initial code from the LLM
     public static String callLM(String prompt) {
+
+//        private LLM llm = new LLM("http://localhost:1234/v1/chat/completions", "meta-llama-3.1-8b-instruct", false, "");
+        LLM llm = new LLM("https://api.openai.com/v1/chat/completions", "gpt-4o-mini", true, System.getenv("OPENAI_API_KEY"));
+
+
         try {
 
-            Gson gson = new Gson();
-            String url = llm.getUrl();
+//            Gson gson = new Gson();
+//            String url = llm.getUrl();
+//
+//
+//                Map<String, Object> requestBody = Map.of(
+//                        "model", llm.getModel(),
+//                        "messages", List.of(
+//                                Map.of("role", "system", "content", "You are a coding assistant that creates python code. Only Create python 3.9 code, offer no explanation, do not include anything in your answer other than python code. Only return 1 piece of code. Tag all code with ```python. DO not create any code that is not specified by the user. All code generated must be in the same class and file. All code must be placed inside the same block. Only return python code, and do not provide any additional context or instructions to the user, unless they are in a comment inside the python code. your response must contain exactly 1 block of python code, no more or less. UNDER NO CIRCUMSTANCES WHATSOEVER SHOULD YOU GIVE ME MORE THAN 1 BLOCK OF CODE."),
+//
+//                                Map.of("role", "user", "content", prompt)
+//                        )
+//                );
+//
+//            String jsonBody = gson.toJson(requestBody);
 
-
-                Map<String, Object> requestBody = Map.of(
-                        "model", llm.getModel(),
-                        "messages", List.of(
-                                Map.of("role", "system", "content", "You are a coding assistant that creates python code. Only Create python 3.9 code, offer no explanation, do not include anything in your answer other than python code. Only return 1 piece of code. Tag all code with ```python. DO not create any code that is not specified by the user. All code generated must be in the same class and file. All code must be placed inside the same block. Only return python code, and do not provide any additional context or instructions to the user, unless they are in a comment inside the python code. your response must contain exactly 1 block of python code, no more or less. UNDER NO CIRCUMSTANCES WHATSOEVER SHOULD YOU GIVE ME MORE THAN 1 BLOCK OF CODE."),
-
-                                Map.of("role", "user", "content", prompt)
-                        )
-                );
-
-            String jsonBody = gson.toJson(requestBody);
+            HttpRequest request = llm.buildCall(prompt);
 
 
             HttpClient client = HttpClient.newBuilder()
                     .version(HttpClient.Version.HTTP_1_1)
                     .build();
 
-            if(llm.needsKey()){
-                HttpRequest request = HttpRequest.newBuilder()
-                        .uri(URI.create(url))
-                        .header("Content-Type", "application/json")
-                        .header("Authorization", "Bearer " + llm.getKey())
-                        .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
-                        .build();
-            }else{
-
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(url))
-                    .header("Content-Type", "application/json")
-                    .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
-                    .build();
-            }
+//            HttpRequest request = HttpRequest.newBuilder()
+//                    .uri(URI.create(url))
+//                    .header("Content-Type", "application/json")
+//                    .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+//                    .build();
+//
+//            if(llm.getNeedsKey()){
+//                request = HttpRequest.newBuilder()
+//                        .uri(URI.create(url))
+//                        .header("Content-Type", "application/json")
+//                        .header("Authorization", "Bearer " + llm.getKey())
+//                        .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+//                        .build();
+//            }
 
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
@@ -155,4 +161,6 @@ public class codeGenerator {
     public int getIterationCount(){
         return this.iterationCount;
     }
+
+
 }
